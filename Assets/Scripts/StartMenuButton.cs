e061f95a6861ff60c3a8b80e8acb9d9569fa87af
飏弟弟 <YangBro@qq.com>
2021-02-03 15:35:29 +0800

    重构了按钮程序

---
+++ Assets/Scripts/StartMenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuButton : MonoBehaviour
+{
+    public void StartOnClick()
+    {
+
+    }
+
+    public void SettingOnClick()
+    {
+
+    }
+
+    public void ExitOnClick()
+    {
+        Application.Quit();
+    }
+}
